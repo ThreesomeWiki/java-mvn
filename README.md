87302de1947a8f513d8f54ec77c45d45587195dc
hanhuizhu <hanhuizhuxyl@gmail.com>
2020-09-13 00:03:07 +0800

    feat:新增okhttp3

---
+++ README.md
@@ -0,0 +1,3 @@
+## 依赖搜索地址
+
+https://search.maven.org/
--- src/main/java/com/dbb/App.java
+++ src/main/java/com/dbb/App.java
@@ -1,5 +1,12 @@
 package com.dbb;
-
+import java.io.IOException;
+import java.lang.Thread;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.jetbrains.annotations.NotNull;
 /**
  * Hello world!
  *
@@ -8,6 +15,26 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        new Thread(new Runnable(){
+            @Override
+            public void run(){
+                try {
+                    OkHttpClient client = new OkHttpClient();
+                    client.newCall(new Request.Builder().url("https://www.baidu.com").build()).enqueue(new Callback(){
+                        @Override
+                        public void onFailure(@NotNull Call call, @NotNull IOException e) {
+
+                        }
+                        @Override
+                        public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                            System.out.println(response.body().string());
+                            System.out.println("this is run");
+                        }
+                    });
+                } catch (Exception e) {
+                    //TODO: handle exception
+                }
+            }
+        }).start();
     }
 }
